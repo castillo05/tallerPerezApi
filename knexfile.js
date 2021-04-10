d4867f69b8ad948c554465d4e771d505aab01c5f
castillo05 <jorgeantonio20142014@gmail.com>
2021-04-10 13:10:49 -0600

    First Commit

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+require('dotenv').config({override:true});
+const path = require('path')
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+
+const BASE_PATH = path.join(__dirname, 'server', 'db');
+
+const options = {
+    connection:{
+            host: DB_HOST,
+			user: DB_USER,
+			password: DB_PASSWORD,
+			database: DB_DATABASE
+    },
+    migrations:{
+        directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds:{
+        directory: path.join(BASE_PATH, 'seeds')
+    }
+}
+
+module.exports=options;
