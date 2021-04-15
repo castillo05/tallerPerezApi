d1ab26d327c9ef81294385663c5b5b4b41e7efa8
castillo05 <jorgeantonio20142014@gmail.com>
2021-04-15 11:25:07 -0600

    Relaciones entre autos y reparaciones

--- server/api/autos/controller.js
+++ server/api/autos/controller.js
@@ -14,7 +14,7 @@ const getAuto=async(req,res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(409).send({ errors: errors.formatWith(formatError).mapped() });
     else {
-    const data = await Auto.find();
+    const data = await Auto.find().withGraphFetched("[reparacion]");
 
     if(!data) return res.status(404).send({error: 'Error getting data'})
 
@@ -123,7 +123,7 @@ const getAutosbyId=async(req,res)=>{
             data: '',
         })
     }
-     const data = await Auto.findById(idAuto);
+     const data = await Auto.findById(idAuto).withGraphFetched("[reparacion]");
  
      if(!data) return res.status(404).send({error: 'Error getting data'})
  
--- server/api/autos/entity.js
+++ server/api/autos/entity.js
@@ -1,4 +1,5 @@
 const Model = require('../model')
+const {Reparacion} = require('../reparaciones/entity');
 class Auto extends Model {
     static get tableName(){
         return 'auto'
@@ -6,6 +7,19 @@ class Auto extends Model {
 
     static get idColumn(){
         return 'idAuto';
+    }
+
+    static get relationMappings(){
+        return {
+            reparacion:{
+                relation:Model.HasManyRelation,
+                modelClass:Reparacion,
+                join:{
+                    from:'auto.idAuto',
+                    to:'reparacion.idAuto'
+                }
+            }
+        }
     }
 }
 
